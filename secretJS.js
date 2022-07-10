4c0d74daba968fa255a221bdc82dcee0bf069169
Sqatcher <kacper.krzywicki@wp.pl>
2022-07-10 12:24:12 +0200

    Updated passwordIgnacyCheck
    Now uses simple encoding

--- secretJS.js
+++ secretJS.js
@@ -2,10 +2,21 @@ function mainHall() {
 	window.location.href = "The_Main_Hall.html";
 }
 
+function magic(word) {
+	let l = word.length;
+	let outcome = word[0];
+	let i = 0;
+	for (i=1; i<l; i++)
+	{
+		outcome = outcome + String.fromCharCode((((word[i-1]).charCodeAt(0) + (word[i]).charCodeAt(0)) % 27) + 97);
+	}
+	return outcome;
+}
+
 function checkIgnacyPassword() {
 	passObject = document.getElementById("lastA");
 	answer = passObject.value;
-	if (answer == "hihiH4Ha")
+	if (magic(answer) == "T{qkgybm{eaojcyw")
 	{
 		document.getElementById("GoodAnswer").style.display = "block";
 		document.getElementById("BadAnswer").style.display = "none";
